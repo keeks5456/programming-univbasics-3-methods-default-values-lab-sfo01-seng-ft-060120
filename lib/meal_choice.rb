408a828f264c2326017254c6762c7069f0473c5c
Akiko Green <akikogreen08@gmail.com>
2020-04-23 00:00:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,7 +2,7 @@
 def meal_choice(veg1, veg2, protein="tofu") 
   puts "What a nutritious meal!"
   meal = "A plate of #{protein} with #{veg1} and #{veg2}."
-  puts "A plate of "
+  puts "A plate of #{veg1} with #{veg2}."
 end
 
 
